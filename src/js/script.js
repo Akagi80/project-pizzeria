a9511fb197810f88c11ed4de4ecb3f92fe2d18c5
Sebastian Sikora <ssikora80@gmail.com>
2021-02-07 20:17:46 +0100

    Add CartProduct.remove and CartProduct.initAction

--- src/js/script.js
+++ src/js/script.js
@@ -495,6 +495,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
       //console.log('thisCartProduct', thisCartProduct);
     }
 
@@ -520,6 +521,34 @@
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });
 
+    }
+
+    remove() {
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions() {
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function(event) {
+        event.preventDefault();
+
+      });
+
+      thisCartProduct.dom.remove.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisCartProduct.remove();
+        console.log('remove', thisCartProduct.remove());
+      });
     }
   }
 
