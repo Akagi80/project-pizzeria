b538f55164591aad817e8048d225a72bbc6f8a35
Sebastian Sikora <ssikora80@gmail.com>
2021-03-06 21:01:43 +0100

    Add getData in Booking.js

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import {templates, select} from '/js/settings.js';
+import {templates, select, settings} from '/js/settings.js';
+import utils from '/js/utils.js';
 import AmountWidget from '/js/components/AmountWidget.js';
 import DatePicker from '/js/components/DatePicker.js';
 import HourPicker from '/js/components/HourPicker.js';
@@ -9,6 +10,65 @@ class Booking {
 
     thisBooking.render(element); // wywołujemy metodę render z dostępem do kontenera (element)
     thisBooking.initWidgets(); // wywołujemy metodę initWidgets
+    thisBooking.getData(); // wywołujemy metodę określania daty rezerwacji
+  }
+
+  getData() {
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking : [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+
+    // console.log('getData params', params);
+
+    const urls = { //obiekt przechowywujący dane rezerwacji podzielony na:
+      booking:       settings.db.url + '/' + settings.db.booking
+                                     + '?' + params.booking.join('&'), // adres endpointu API, który zwraca listę rezerwacji
+      eventsCurrent: settings.db.url + '/' + settings.db.event
+                                     + '?' + params.eventsCurrent.join('&'), // zwraca listę wydarzeń (rezerwacji) jednorazowych
+      eventsRepeat:  settings.db.url + '/' + settings.db.event
+                                     + '?' + params.eventsRepeat.join('&'), // zwraca listę wydarzeń (rezerwacji) cyklicznych
+    };
+
+    // console.log('getData urls', urls);
+
+    Promise.all([
+      fetch(urls.booking), // pobieramy z API informacje o rezerwacji
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses) {
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all ([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]) {
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
+
   }
 
   render(element) {
@@ -23,8 +83,8 @@ class Booking {
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount); // dostęp do inputu hoursAmount
     thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
-    console.log(thisBooking.dom.datePicker);
-    console.log(thisBooking.dom.hourPicker);
+    // console.log(thisBooking.dom.datePicker);
+    // console.log(thisBooking.dom.hourPicker);
   }
 
   initWidgets() {
